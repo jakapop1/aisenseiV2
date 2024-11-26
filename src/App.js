aebd01f3ae533bafd48adfc05292ddcc91da5eb2
Sean Umeda <seankumeda@gmail.com>
2024-11-25 14:33:14 -1000

    Adding content pages for LPW Setup, deleted unused pages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/layout/Navigation';
 import Home from './pages/Home';
 import LPWLanding from './pages/lpw/LPWLanding';
-import LPWPythonWireshark from './pages/lpw/LPWPythonWireshark';
-import OllamaSetup from './pages/lpw/OllamaSetup';
+import LPWPythonWireshark from './pages/lpw/LPWSetup/LPWPythonWireshark';
+import OllamaSetup from './pages/lpw/LPWSetup/OllamaSetup';
+import VirtualEnvironmentSetup from './pages/lpw/LPWSetup/VitrualEnvironmentSetup';
+import LPWConfiguration from './pages/lpw/LPWSetup/LPWConfiguration';
+import UsingLPW from './pages/lpw/LPWSetup/UsingLPW';
+import UnderstandingLLMs from './pages/lpw/Exercises/UnderstandingLLMS';
+import ModelSelection from './pages/lpw/Exercises/ModelSelection';
+import PromptEngineering from './pages/lpw/Exercises/PromptEngineering';
 
 function App() {
   return (
@@ -18,6 +24,12 @@ function App() {
             <Route path="/lpw/introduction" element={<LPWLanding />} />
             <Route path="/lpw/python-wireshark" element={<LPWPythonWireshark />} />
             <Route path="/lpw/ollama" element={<OllamaSetup />} />
+            <Route path="/lpw/venvsetup" element={<VirtualEnvironmentSetup />} />
+            <Route path="/lpw/configuration" element={<LPWConfiguration />} />
+            <Route path="/lpw/using" element={<UsingLPW />} />
+            <Route path="/lpw/UnderstandingLLMs" element={<UnderstandingLLMs />} />
+            <Route path="/lpw/ModelSelection" element={<ModelSelection />} />
+            <Route path="/lpw/PromptEngineering" element={<PromptEngineering />} />
           </Routes>
         </main>
       </div>
